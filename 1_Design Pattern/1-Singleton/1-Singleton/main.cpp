ea3d4b50831ffd9139fca30798871c009ce092a3
JCS <popo6511@gmail.com>
2022-05-27 00:19:24 +0900

    Revert "Revert "[1-1] Singleton sample_220526""
    This reverts commit 8a185fab9144b24be2d5c0102e0c5a62627b36b0.

---
+++ 1_Design Pattern/1-Singleton/1-Singleton/main.cpp
@@ -0,0 +1,29 @@
+﻿#define CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+#include "Singleton.h"
+
+
+int main()
+{
+	Singleton &a = Singleton::instance();
+	a.increaseInt();	// nInt : 0 -> 5
+	a.printInt();
+	Singleton &b = Singleton::instance();
+	b.increaseInt();	// nInt : 5 -> 10
+	a.printInt();
+	b.printInt();
+
+	a.increaseInt();	// nInt : 10 -> 15
+
+	a.printInt();
+	b.printInt();
+
+	b.increaseInt();	// nInt : 15 -> 20
+
+	a.printInt();
+	b.printInt();
+
+	return 0;
+}
+
